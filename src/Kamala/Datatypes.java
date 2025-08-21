b0ee72916ba488a5138e02a93a682240809ff7bc
Kamela Sadat <kamela.sadat1990@gmail.com>
2025-08-21 17:37:19 -0400

    first commit one

---
+++ src/Kamala/Datatypes.java
@@ -0,0 +1,18 @@
+package Kamala;
+
+public class Datatypes {
+
+
+    public static void main(String[] args) {
+
+
+        int numbers = 123;
+
+        boolean result = false;
+
+        char charaters = 'a';
+
+        String name = "Kamlal";
+
+    }
+}
