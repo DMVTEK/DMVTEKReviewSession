4fb1bada95f8bc42739abafc91c97c237e427835
DMVTEK <hamid@dmvtek.com>
2026-01-07 20:06:55 -0500

    Req002

---
+++ src/test/java/Requrements.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class Requrements {
+
+     WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void req001() {
+        String expectedTitle = "DEMOQA";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        WebElement elementButton = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        WebElement alertFrameWindowButton = driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']"));
+
+        Assert.assertTrue(elementButton.isDisplayed());
+        Assert.assertTrue(alertFrameWindowButton.isDisplayed());
+
+    }
+
+    @Test
+    public void req002() {
+        WebElement elementButton = driver.findElement(By.xpath("//h5[text()='Elements']"));
+        WebElement alertFrameWindowButton = driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']"));
+        WebElement formsButton = driver.findElement(By.xpath("//h5[text()='Forms']"));
+        WebElement WidgetsButton = driver.findElement(By.xpath("//h5[text()='Widgets']"));
+        WebElement interactionsButton = driver.findElement(By.xpath("//h5[text()='Interactions']"));
+        WebElement bookStoreApplicationButton = driver.findElement(By.xpath("//h5[text()='Book Store Application']"));
+        Assert.assertTrue(elementButton.isDisplayed());
+        Assert.assertTrue(alertFrameWindowButton.isDisplayed());
+        Assert.assertTrue(formsButton.isDisplayed());
+        Assert.assertTrue(interactionsButton.isDisplayed());
+        Assert.assertTrue(WidgetsButton.isDisplayed());
+        Assert.assertTrue(bookStoreApplicationButton.isDisplayed());
+
+    }
+}
